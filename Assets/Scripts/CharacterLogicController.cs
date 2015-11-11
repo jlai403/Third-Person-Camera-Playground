2420182c4875fe93e0078dbdd70fce40c5e502f1
Joey Lai <joey.lai@knelf.com>
2015-11-10 20:08:39 -0700

    Initial commit
    - Animations, Prefabs, Environments, Textures
    - Character movement

---
+++ Assets/Scripts/CharacterLogicController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterLogicController : MonoBehaviour {
+
+	[SerializeField]
+	private Animator animator;
+	[SerializeField]
+	private float directionDampTime = .25f;
+
+	private float speed = 0.0f;
+	private float horizontal = 0.0f;
+	private float vertical = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+
+		if (animator.layerCount >= 2) {
+			animator.SetLayerWeight (1, 1);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (animator) {
+			horizontal = Input.GetAxis("Horizontal");
+			vertical = Input.GetAxis("Vertical");
+			speed = new Vector2(horizontal, vertical).sqrMagnitude;
+
+			Debug.Log ("horizontal: " + horizontal);
+			Debug.Log ("vertical: " + vertical);
+
+			animator.SetFloat("Speed", speed);
+			animator.SetFloat("Direction", horizontal, directionDampTime, Time.deltaTime);
+		}
+	}
+}
